93c0f3c2af068b143272d73687b77fd765fc071c
Nikola Bunjevac <nikola.bunjevac@gmail.com>
2024-04-14 01:40:04 +0200

    Add font and cloud effect

--- Scripts/Platform.gd
+++ Scripts/Platform.gd
@@ -2,19 +2,22 @@ extends AnimatableBody2D
 
 
 @export var speed: float = 150
-@export var vanish_time: float = 1
+@export var vanish_time: float = 2
+@export var cutoff_scale: float = 0.5
 
 
 var summoned_by_user = false
 
 
-func _physics_process(delta):
-	position.x -= speed * delta
-	
+func _ready():
+	$CollisionShape2D/CPUParticles2D.lifetime = vanish_time
 	if summoned_by_user:
-		scale.x = max(0, scale.x - delta * 1 / vanish_time)
-	
-	if position.x < -2000 or scale.x <= 0.01:
+		var tween = create_tween()
+		tween.tween_property(self, "scale", Vector2(cutoff_scale, 1), vanish_time)
+
+
+func _physics_process(delta):
+	if scale.x <= cutoff_scale:
 		get_parent().remove_child(self)
 		queue_free()
 	
