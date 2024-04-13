3ae9cc4369d47c72d1e47029bcab4fbe0a1cfba5
Nikola Bunjevac <nikola.bunjevac@gmail.com>
2024-04-13 15:19:40 +0200

    Progress

--- Scenes/Level.gd
+++ Scenes/Level.gd
@@ -1,19 +1,34 @@
 extends Node2D
 
 
+@export var platform_summon_cooldown: float = 1.0
+
+var can_summon_platform = true
+
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	pass # Replace with function body.
 
 
-# Called every frame. 'delta' is the elapsed time since the previous frame.
+func add_platform(at_pos, summoned_by_user=false):
+	var platform = $Platform.duplicate()
+	platform.position = at_pos
+	platform.summoned_by_user = summoned_by_user
+	platform.process_mode = Node.PROCESS_MODE_INHERIT
+	add_child(platform)
+
+
 func _process(delta):
-	pass
+	if Input.is_action_pressed("ui_down") and can_summon_platform:
+		add_platform($Player.position + Vector2(0, 10), true)
+		can_summon_platform = false
+		$PlatformSummonCooldown.start(platform_summon_cooldown)
 
 
 func _on_timer_timeout():
-	var platform = $Platform.duplicate()
-	platform.position.x = 1500
-	platform.position.y = 500 + randi_range(-200, 150)
-	platform.process_mode = Node.PROCESS_MODE_INHERIT
-	add_child(platform)
+	var at_pos = Vector2(1500, 500 + randi_range(-200, 150))
+	add_platform(at_pos, false)
+
+
+func _on_platform_summon_cooldown_timeout():
+	can_summon_platform = true
--- Scenes/Platform.gd
+++ Scenes/Platform.gd
@@ -2,12 +2,19 @@ extends AnimatableBody2D
 
 
 @export var speed: float = 150
+@export var vanish_time: float = 1
+
+
+var summoned_by_user = false
 
 
 func _physics_process(delta):
 	position.x -= speed * delta
 	
-	if position.x < -2000:
+	if summoned_by_user:
+		scale.x = max(0, scale.x - delta * 1 / vanish_time)
+	
+	if position.x < -2000 or scale.x <= 0.01:
 		get_parent().remove_child(self)
 		queue_free()
 	
