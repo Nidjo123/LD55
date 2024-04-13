858b61e7b6919d6798c39cb05ceb8d6546d7e62c
Nikola Bunjevac <nikola.bunjevac@gmail.com>
2024-04-13 11:53:58 +0200

    Add placeholder cat image and test level

--- Scripts/Player.gd
+++ Scripts/Player.gd
@@ -2,24 +2,26 @@ extends CharacterBody2D
 
 
 @export var speed: float = 300.0
-@export var jump_velocity: float = -400.0
+@export var jump_velocity: float = -600.0
 
 # Get the gravity from the project settings to be synced with RigidBody nodes.
-var gravity = ProjectSettings.get_setting("physics/2d/default_gravity")
+var gravity = 1500 #ProjectSettings.get_setting("physics/2d/default_gravity")
+
+
+func _process(delta):
+	var direction = Input.get_axis("ui_left", "ui_right")
+	if direction:
+		$AnimatedSprite2D.flip_h = direction < 0 
 
 
 func _physics_process(delta):
-	# Add the gravity.
 	if not is_on_floor():
 		velocity.y += gravity * delta
 
-	# Handle jump.
-	if Input.is_action_just_pressed("ui_accept") and is_on_floor():
+	if Input.is_action_just_pressed("jump") and is_on_floor():
 		velocity.y = jump_velocity
 
-	# Get the input direction and handle the movement/deceleration.
-	# As good practice, you should replace UI actions with custom gameplay actions.
-	var direction = Input.get_axis("ui_left", "ui_right")
+	var direction = Input.get_axis("left", "right")
 	if direction:
 		velocity.x = direction * speed
 	else:
