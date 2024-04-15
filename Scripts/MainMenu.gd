fc30352542d8269c74b18f441e98d50c2671972a
Nikola Bunjevac <nikola.bunjevac@gmail.com>
2024-04-15 13:53:51 +0200

    New level, sounds, animations

--- Scripts/MainMenu.gd
+++ Scripts/MainMenu.gd
@@ -2,11 +2,15 @@ extends Control
 
 
 func _ready():
+	var music_node = $"/root/MusicTheme"
+	music_node.stream = load("res://Art/Audio/menu_theme.wav")
+	music_node.play()
 	$VBoxContainer/PlayButton.grab_focus()
 
 
 func _on_play_button_pressed():
 	var music = get_node('/root/MusicTheme')
+	music.stream = load("res://Art/Audio/theme.wav")
 	music.play()
 	get_tree().change_scene_to_file("res://Scenes/Level.tscn")
 
